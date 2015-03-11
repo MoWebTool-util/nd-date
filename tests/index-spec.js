25a124beb27e537f18969beefa8ce06491c48778
lzhengms <lzhengms@gmail.com>
2015-03-11 13:27:01 +0800

    add

---
+++ tests/index-spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('expect.js');
+var Date = require('../index');
+
+/*globals describe, it*/
+
+describe('nd-date', function() {
+
+  it('normal usage', function() {
+
+  });
+
+});
